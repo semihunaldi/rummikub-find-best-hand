d7951f475526222f606b98328080d53bc10799ec
semihunaldi <semihunaldi@gmail.com>
2018-04-13 20:08:49 +0300

    factory pattern implemented properly

--- src/test/java/com/semihunaldi/rummikub/HandDistributionFactoryTest.java
+++ src/test/java/com/semihunaldi/rummikub/HandDistributionFactoryTest.java
@@ -14,52 +14,49 @@ import java.util.stream.Collectors;
  */
 public class HandDistributionFactoryTest {
 
+	private HandDistributionFactory handDistributionFactory = new HandDistributionFactory();
+
 	@Test
 	public void testHandDistribution() {
-		HandDistributionFactory handDistributionFactory = new HandDistributionFactory();
-		handDistributionFactory.distribute();
-		Assertions.assertThat(handDistributionFactory.getPlayer1().size()).isEqualTo(15);
-		Assertions.assertThat(handDistributionFactory.getPlayer2().size()).isEqualTo(14);
-		Assertions.assertThat(handDistributionFactory.getPlayer3().size()).isEqualTo(14);
-		Assertions.assertThat(handDistributionFactory.getPlayer4().size()).isEqualTo(14);
-		Assertions.assertThat(handDistributionFactory.getRemainingTiles().size()).isEqualTo(HandDistributionFactory.totalTileCount - 15 - 14 - 14 - 14);
+		HandDistribution handDistribution = handDistributionFactory.distributeHand();
+		Assertions.assertThat(handDistribution.getPlayer1().size()).isEqualTo(15);
+		Assertions.assertThat(handDistribution.getPlayer2().size()).isEqualTo(14);
+		Assertions.assertThat(handDistribution.getPlayer3().size()).isEqualTo(14);
+		Assertions.assertThat(handDistribution.getPlayer4().size()).isEqualTo(14);
+		Assertions.assertThat(handDistribution.getRemainingTiles().size()).isEqualTo(handDistributionFactory.getTotalTileCount() - 15 - 14 - 14 - 14);
 	}
 
 	@Test
 	public void testTilesForNulls() {
-		HandDistributionFactory handDistributionFactory = new HandDistributionFactory();
-		handDistributionFactory.distribute();
-		testTiles(handDistributionFactory.getPlayer1());
-		testTiles(handDistributionFactory.getPlayer2());
-		testTiles(handDistributionFactory.getPlayer3());
-		testTiles(handDistributionFactory.getPlayer4());
+		HandDistribution handDistribution = handDistributionFactory.distributeHand();
+		testTiles(handDistribution.getPlayer1());
+		testTiles(handDistribution.getPlayer2());
+		testTiles(handDistribution.getPlayer3());
+		testTiles(handDistribution.getPlayer4());
 	}
 
 	@Test
 	public void testTileNumberRanges() {
-		HandDistributionFactory handDistributionFactory = new HandDistributionFactory();
-		handDistributionFactory.distribute();
-		List<Tile> tiles = joinAll(handDistributionFactory);
+		HandDistribution handDistribution = handDistributionFactory.distributeHand();
+		List<Tile> tiles = joinAll(handDistribution);
 		for(Tile tile : tiles){
-			Assertions.assertThat(tile.getNumber()).isGreaterThanOrEqualTo(HandDistributionFactory.minNum);
-			Assertions.assertThat(tile.getNumber()).isLessThanOrEqualTo(HandDistributionFactory.maxNum);
+			Assertions.assertThat(tile.getNumber()).isGreaterThanOrEqualTo(handDistributionFactory.getMinNum());
+			Assertions.assertThat(tile.getNumber()).isLessThanOrEqualTo(handDistributionFactory.getMaxNum());
 		}
 	}
 
 	@Test
 	public void testNumberOfFakes() {
-		HandDistributionFactory handDistributionFactory = new HandDistributionFactory();
-		handDistributionFactory.distribute();
-		List<Tile> tiles = joinAll(handDistributionFactory);
+		HandDistribution handDistribution = handDistributionFactory.distributeHand();
+		List<Tile> tiles = joinAll(handDistribution);
 		List<Tile> fakes = tiles.stream().filter(Tile::isFake).collect(Collectors.toList());
-		Assertions.assertThat(fakes.size()).isEqualTo(HandDistributionFactory.numberOfFakes);
+		Assertions.assertThat(fakes.size()).isEqualTo(handDistributionFactory.getNumberOfFakes());
 	}
 
 	@Test
 	public void testNumberOfJokers() {
-		HandDistributionFactory handDistributionFactory = new HandDistributionFactory();
-		handDistributionFactory.distribute();
-		List<Tile> tiles = joinAll(handDistributionFactory);
+		HandDistribution handDistribution = handDistributionFactory.distributeHand();
+		List<Tile> tiles = joinAll(handDistribution);
 		List<Tile> jokers = tiles.stream().filter(Tile::isJoker).collect(Collectors.toList());
 		Assertions.assertThat(jokers.size()).isEqualTo(2);
 	}
@@ -73,28 +70,32 @@ public class HandDistributionFactoryTest {
 	}
 
 	@Test
-	public void loadTest() {
+	public void testJokerFound() {
+		HandDistribution handDistribution = handDistributionFactory.distributeHand();
+		Assertions.assertThat(handDistribution.getJoker()).isNotNull();
+	}
+
+	@Test
+	public void testLoad() {
 		for(int i = 0; i < 100_000; i++){
-			HandDistributionFactory handDistributionFactory = new HandDistributionFactory();
-			handDistributionFactory.distribute();
+			handDistributionFactory.distributeHand();
 		}
 	}
 
 	private void testColorCounts(TileColor color) {
-		HandDistributionFactory handDistributionFactory = new HandDistributionFactory();
-		handDistributionFactory.distribute();
-		List<Tile> tiles = joinAll(handDistributionFactory);
+		HandDistribution handDistribution = handDistributionFactory.distributeHand();
+		List<Tile> tiles = joinAll(handDistribution);
 		List<Tile> colors = tiles.stream().filter(tile -> tile.getTileColor().equals(color) && !tile.isFake()).collect(Collectors.toList());
 		Assertions.assertThat(colors.size()).isEqualTo(26);
 	}
 
-	private List<Tile> joinAll(HandDistributionFactory handDistributionFactory) {
+	private List<Tile> joinAll(HandDistribution handDistribution) {
 		List<Tile> all = Lists.newArrayList();
-		all.addAll(handDistributionFactory.getPlayer1());
-		all.addAll(handDistributionFactory.getPlayer2());
-		all.addAll(handDistributionFactory.getPlayer3());
-		all.addAll(handDistributionFactory.getPlayer4());
-		all.addAll(handDistributionFactory.getRemainingTiles());
+		all.addAll(handDistribution.getPlayer1());
+		all.addAll(handDistribution.getPlayer2());
+		all.addAll(handDistribution.getPlayer3());
+		all.addAll(handDistribution.getPlayer4());
+		all.addAll(handDistribution.getRemainingTiles());
 		return all;
 	}
 
